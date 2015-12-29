745726586522fd49004c81040e990c82724fb827
Alessio Santo <alessio.santocs@gmail.com>
2015-12-29 14:54:03 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gistscript::Application.config.secret_key_base = 'a828ab33a2fedd732988d1254e53f63b2f36778646cfdb7796e20810cb875520dce0c57f9a41e9e6fcec9283f166e3d7d127e8c138ecf62b2b1e7c299888aaa4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gistscript::Application.config.session_store :cookie_store, key: '_gistscript_session'
